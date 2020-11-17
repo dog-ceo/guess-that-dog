d4a3952f505d2c8bfe788aa7682892437c0b44ba
Ben <ben.speakman@peakon.com>
2020-11-17 00:35:57 +0000

    Tidy up

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,84 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import * as tf from '@tensorflow/tfjs';
+import { GraphModel } from '@tensorflow/tfjs';
 import breeds from './lib/breeds';
 import './App.css';
 
 function App() {
+  const [model, setModel] = useState<GraphModel>();
   const [imgSrc, setImgSrc] = useState('');
   const [message, setMessage] = useState('');
   const fileInput = React.useRef<HTMLInputElement>();
 
-  async function handleClick() {
+  useEffect(() => {
+    (async function loadModel() {
+      setModel(await tf.loadGraphModel('model/1/model.json'));
+    })();
+  }, []);
+
+  function handleClick() {
     fileInput.current.click()
   }
 
-  async function handleChange(event: ChangeEvent<HTMLInputElement>) {
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
     setMessage('Ummmm.....');
 
-    const model = await tf.loadGraphModel('model/1/model.json');
     const reader = new FileReader();
-    const image = document.getElementById("img") as HTMLImageElement;
 
-    reader.onload = async (event) => {
+    reader.onload = (event) => {
       setImgSrc(event.target.result as string);
-
-      document.getElementById('main').style.backgroundImage = `url(${ event.target.result })`;
-
-      image.onload = async () => {
-        const inputTensor = tf.browser.fromPixels(image);
-        const resized = tf.image.resizeBilinear(inputTensor, [224, 224]).toFloat();
-        const offset = tf.scalar(255.0);
-        const normalized = resized.div(offset);
-        const batched = normalized.expandDims(0);
-
-        const outputTensor = await model.predict(batched) as tf.Tensor;
-        const prediction = await outputTensor.data();
-
-        let guess = Array.from(prediction).map((probability, index) => ({
-          probability,
-          breed: breeds[index - 1]
-        })).sort((a,b) => b.probability-a.probability).slice(0,1)[0];
-
-        const confidence = Math.round(guess.probability * 100);
-
-        let word;
-
-        if (confidence >= 90) {
-          word = 'pretty'
-        } else if (confidence >= 80) {
-          word = 'quite'
-        } else if (confidence >= 60) {
-          word = 'kinda'
-        } else if (confidence >= 40) {
-          word = 'vaguely'
-        } else if (confidence >= 20) {
-          word = 'only slightly'
-        } else {
-          word = 'only a little bit'
-        }
-
-        setMessage(`So I'm ${word} sure that is a ${guess.breed}?`);
-      }
     };
 
     reader.readAsDataURL(event.target.files[0]);
   }
 
+  async function handleLoad(event: ChangeEvent<HTMLImageElement>) {
+    const image = event.target;
+    const inputTensor = tf.browser.fromPixels(image);
+    const resized = tf.image.resizeBilinear(inputTensor, [224, 224]).toFloat();
+    const offset = tf.scalar(255.0);
+    const normalized = resized.div(offset);
+    const batched = normalized.expandDims(0);
+
+    const outputTensor = await model.predict(batched) as tf.Tensor;
+    const prediction = await outputTensor.data();
+
+    const guesses = Array.from(prediction).map((probability, index) => ({
+      probability,
+      breed: breeds[index - 1]
+    })).sort((a,b) => b.probability - a.probability);
+
+    const bestGuess = guesses[0];
+    const confidence = Math.round(bestGuess.probability * 100);
+
+    let confidenceWord;
+
+    if (confidence >= 90) {
+      confidenceWord = 'pretty'
+    } else if (confidence >= 80) {
+      confidenceWord = 'quite'
+    } else if (confidence >= 60) {
+      confidenceWord = 'kinda'
+    } else if (confidence >= 40) {
+      confidenceWord = 'vaguely'
+    } else if (confidence >= 20) {
+      confidenceWord = 'only slightly'
+    } else {
+      confidenceWord = 'only a little bit'
+    }
+
+    setMessage(`So I'm ${confidenceWord} sure that is a ${bestGuess.breed}?`);
+  }
+
   return (
     <div>
-      <div id="main">
+      <div id="main" style={{ backgroundImage: `url(${imgSrc})` }}>
         <div className="controls">
           <input type='file' ref={fileInput} onChange={handleChange} />
           <button id='upload' onClick={handleClick} >Submit a dog image</button>
           <div id="result">{message}</div>
         </div>
-        <img alt="dog" id="img" src={imgSrc}/>
+        <img alt="dog" id="img" src={imgSrc} onLoad={handleLoad} />
       </div>
     </div>
   );
